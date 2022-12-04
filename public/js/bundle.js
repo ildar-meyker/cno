8a0f84140da82233582ce559a976a2eeaaf23b36
Ildar Meyker <ildar.meyker@gmail.com>
2022-12-04 22:48:51 +0300

    Improve teachers section.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -41,10 +41,27 @@ $(".slider-faces").each(function () {
   $(this).find(".slider-faces__list").slick({
     dots: true,
     infinite: false,
+    autoplay: false,
     prevArrow: $(this).find(".slider-faces__prev"),
     nextArrow: $(this).find(".slider-faces__next"),
     appendDots: $(this).find(".slider-faces__dots"),
-    slidesToShow: 4
+    slidesToShow: 4,
+    responsive: [{
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
   });
 });
 
--- src/js/modules/sliderFaces.js
+++ src/js/modules/sliderFaces.js
@@ -4,9 +4,30 @@ $(".slider-faces").each(function () {
 		.slick({
 			dots: true,
 			infinite: false,
+			autoplay: false,
 			prevArrow: $(this).find(".slider-faces__prev"),
 			nextArrow: $(this).find(".slider-faces__next"),
 			appendDots: $(this).find(".slider-faces__dots"),
 			slidesToShow: 4,
+			responsive: [
+				{
+					breakpoint: 900,
+					settings: {
+						slidesToShow: 3,
+					},
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 2,
+					},
+				},
+				{
+					breakpoint: 400,
+					settings: {
+						slidesToShow: 1,
+					},
+				},
+			],
 		});
 });
