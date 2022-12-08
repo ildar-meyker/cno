6084afbf07c7b77f6e0ed5f7b19d5c6f6e06ea17
Ildar Meyker <ildar.meyker@gmail.com>
2022-12-08 16:58:45 +0300

    Replace accordion by accordion-2.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-import "../../node_modules/bootstrap/js/dist/util";
-import "../../node_modules/bootstrap/js/dist/collapse";
 import "./modules/rangePicker";
 import "./modules/sliderPeople";
 import "./modules/sliderReviews";
